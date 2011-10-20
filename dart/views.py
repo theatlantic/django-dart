a2b623dc208cecca450c52a3afb29fc06e9ba886
Josh West <joshwest@theatlantic.com>
2011-10-20 16:25:09 -0400

    Removed unused import

--- dart/views.py
+++ dart/views.py
@@ -1,8 +1,7 @@
 from coffin.shortcuts import render_to_response
 from coffin.template import RequestContext
-from blogcore.pagecache import cache_page
 
-def ad(request,ad_url):
+def ad(request, ad_url):
 	"""Function to display an ad.  Currently customized for the thankyou for sharing iframe ad."""
 	# truncating this so it can't be passed an arbitrarily long string
 	ad_url = 'http://ad.doubleclick.net/adj/'+ad_url[:500]
