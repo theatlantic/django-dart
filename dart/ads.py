c61bb59191de24c46e68d516a9c087a85f140f6e
Chris Barna <chris@unbrain.net>
2013-04-17 09:38:29 -0400

    Arduous march: Retab all the things!

--- dart/ads.py
+++ dart/ads.py
@@ -10,124 +10,124 @@ from django.conf import settings
 DART_AD_DEFAULTS = getattr(settings, 'DART_AD_DEFAULTS', {})
 
 if hasattr(DART_AD_DEFAULTS, 'site'):
-	DEFAULT_SITE = DART_AD_DEFAULTS['site']
-	del(DART_AD_DEFAULTS['site'])
+    DEFAULT_SITE = DART_AD_DEFAULTS['site']
+    del(DART_AD_DEFAULTS['site'])
 else:
-	DEFAULT_SITE = 'site'
+    DEFAULT_SITE = 'site'
 
 if hasattr(DART_AD_DEFAULTS, 'zone'):
-	DEFAULT_SITE = DART_AD_DEFAULTS['zone']
-	del(DART_AD_DEFAULTS['zone'])
+    DEFAULT_SITE = DART_AD_DEFAULTS['zone']
+    del(DART_AD_DEFAULTS['zone'])
 else:
-	DEFAULT_ZONE = 'zone'
+    DEFAULT_ZONE = 'zone'
 
 if hasattr(DART_AD_DEFAULTS, 'dfp_id'):
-	DEFAULT_ID = DART_AD_DEFAULTS['dfp_id']
-	del(DART_AD_DEFAULTS['dfp_id'])
+    DEFAULT_ID = DART_AD_DEFAULTS['dfp_id']
+    del(DART_AD_DEFAULTS['dfp_id'])
 else:
-	DEFAULT_ID = None
+    DEFAULT_ID = None
 
 class Ad(object):
-	""" Base class for Ad and Ad_Factory, keeps track of assigned attributes """
+    """ Base class for Ad and Ad_Factory, keeps track of assigned attributes """
 
-	default_site = DEFAULT_SITE
-	default_zone = DEFAULT_ZONE
-	default_id = DEFAULT_ID
+    default_site = DEFAULT_SITE
+    default_zone = DEFAULT_ZONE
+    default_id = DEFAULT_ID
 
-	def __init__(self, pos, size='0x0', desc_text='', template='ad.html',**kwargs):
-		
-		try:
-			self.site = kwargs['site']
-			del(kwargs['site'])
-		except KeyError:
-			self.site = self.default_site
+    def __init__(self, pos, size='0x0', desc_text='', template='ad.html',**kwargs):
 
-		try:
-			self.zone = kwargs['zone']
-			del(kwargs['zone'])
-		except KeyError:
-			self.zone = self.default_zone
+        try:
+            self.site = kwargs['site']
+            del(kwargs['site'])
+        except KeyError:
+            self.site = self.default_site
 
-		try:
-			self.dfp_id = kwargs['dfp_id']
-			del(kwargs['dfp_id'])
-		except:
-			self.dfp_id = self.default_id
-		
-		
-		self.desc_text = desc_text
-		self.template = template
+        try:
+            self.zone = kwargs['zone']
+            del(kwargs['zone'])
+        except KeyError:
+            self.zone = self.default_zone
 
-		self.attributes = {}
-		self.attributes.update(kwargs)
-		self.attributes['pos'] = pos
-		self.attributes['sz'] = size
+        try:
+            self.dfp_id = kwargs['dfp_id']
+            del(kwargs['dfp_id'])
+        except:
+            self.dfp_id = self.default_id
 
-	def get_zone(self):
-		return self._zone
 
-	def set_zone(self, value):
-		self._zone = slugify(value)
+        self.desc_text = desc_text
+        self.template = template
 
-	zone = property(get_zone, set_zone)
+        self.attributes = {}
+        self.attributes.update(kwargs)
+        self.attributes['pos'] = pos
+        self.attributes['sz'] = size
 
-	def get_link(self):
-		link = '%s/%s;' % (self.site, self.zone)
+    def get_zone(self):
+        return self._zone
 
-		for attr, val in self.attributes.items():
+    def set_zone(self, value):
+        self._zone = slugify(value)
 
-			# if it is a non-string iteratible object
-			if hasattr(val, '__iter__'):
-				link += self._format_multiple_values(attr, val)
-			else:
-				link += self._format_value(attr, val)
+    zone = property(get_zone, set_zone)
 
-		return link
+    def get_link(self):
+        link = '%s/%s;' % (self.site, self.zone)
 
-	def _format_value(self, attribute_name, val):
+        for attr, val in self.attributes.items():
 
-		if attribute_name != 'sz':
-			val = slugify(val)
+            # if it is a non-string iteratible object
+            if hasattr(val, '__iter__'):
+                link += self._format_multiple_values(attr, val)
+            else:
+                link += self._format_value(attr, val)
 
-		return "%s=%s;" % (attribute_name, val)
+        return link
 
-	def _format_multiple_values(self, attr, values):
-		formatted = u''
-		for val in values:
-			formatted = u'%s%s' % (formatted, self._format_value(attr, val))
-		return formatted
+    def _format_value(self, attribute_name, val):
 
-	def __unicode__(self):
-		""" Prints out the Ad using the ad.html template """
+        if attribute_name != 'sz':
+            val = slugify(val)
 
-		link = self.get_link()
-		t = get_template(self.template)
-		c = Context({
-			'pos': self.attributes['pos'],
-			'link': link,
-			'dfp_id': '/%s' % self.dfp_id if self.dfp_id is not None else '',
-			'desc_text': self.desc_text
-		})
-		return t.render(c)
+        return "%s=%s;" % (attribute_name, val)
+
+    def _format_multiple_values(self, attr, values):
+        formatted = u''
+        for val in values:
+            formatted = u'%s%s' % (formatted, self._format_value(attr, val))
+        return formatted
+
+    def __unicode__(self):
+        """ Prints out the Ad using the ad.html template """
+
+        link = self.get_link()
+        t = get_template(self.template)
+        c = Context({
+            'pos': self.attributes['pos'],
+            'link': link,
+            'dfp_id': '/%s' % self.dfp_id if self.dfp_id is not None else '',
+            'desc_text': self.desc_text
+        })
+        return t.render(c)
 
 class AdFactory(object):
 
-	attributes = {}
-	default_attributes = DART_AD_DEFAULTS
+    attributes = {}
+    default_attributes = DART_AD_DEFAULTS
 
-	def __init__(self, **kwargs):
-		self.attributes = self.default_attributes.copy()
-		self.set(**kwargs)
+    def __init__(self, **kwargs):
+        self.attributes = self.default_attributes.copy()
+        self.set(**kwargs)
 
 
-	def set(self, **kwargs):
-		self.attributes.update(kwargs)
+    def set(self, **kwargs):
+        self.attributes.update(kwargs)
 
-	def get(self, *args, **kwargs):
+    def get(self, *args, **kwargs):
 
-		attr = self.attributes.copy()
-		attr.update(kwargs)
+        attr = self.attributes.copy()
+        attr.update(kwargs)
 
-		ad = Ad(*args, **attr)
-		
-		return ad
+        ad = Ad(*args, **attr)
+
+        return ad
--- dart/templatetags/ad_tags.py
+++ dart/templatetags/ad_tags.py
@@ -4,11 +4,11 @@ from dart.ads import Ad, AdFactory
 register = template.Library()
 
 def get_ad(**kwargs):
-	return Ad(**kwargs)
+    return Ad(**kwargs)
 
 register.object(get_ad)
 
 def get_adfactory(**kwargs):
-	return AdFactory(**kwargs)
+    return AdFactory(**kwargs)
 
 register.object(get_adfactory)
