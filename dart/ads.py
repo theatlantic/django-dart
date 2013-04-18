085e90a1cfc81accb5c4274b53c2f53d4f856f9b
Chris Barna <chris@unbrain.net>
2013-04-18 11:05:20 -0400

    First pass at GPT.

--- dart/ads.py
+++ dart/ads.py
@@ -1,6 +1,7 @@
-from random import randint
+import collections
 
 from django.template.defaultfilters import slugify
+from django.db.models.query import QuerySet
 
 from coffin.template import Context
 from coffin.template.loader import get_template
@@ -9,59 +10,47 @@ from django.conf import settings
 
 DART_AD_DEFAULTS = getattr(settings, 'DART_AD_DEFAULTS', {})
 
-if hasattr(DART_AD_DEFAULTS, 'site'):
-    DEFAULT_SITE = DART_AD_DEFAULTS['site']
-    del(DART_AD_DEFAULTS['site'])
-else:
-    DEFAULT_SITE = 'site'
+# This sets DEFAULT_SITE, DEFAULT_ZONE, DEFAULT_DFP_ID
+for DEFAULT_KEY in ['site', 'zone', 'dfp_id']:
+    if hasattr(DART_AD_DEFAULTS, DEFAULT_KEY):
+        globals()['DEFAULT_{0}'.format(DEFAULT_KEY.upper())] = DART_AD_DEFAULTS[DEFAULT_KEY]
+        del(DART_AD_DEFAULTS[DEFAULT_KEY])
+    else:
+        globals()['DEFAULT_{0}'.format(DEFAULT_KEY.upper())] = DEFAULT_KEY
 
-if hasattr(DART_AD_DEFAULTS, 'zone'):
-    DEFAULT_SITE = DART_AD_DEFAULTS['zone']
-    del(DART_AD_DEFAULTS['zone'])
-else:
-    DEFAULT_ZONE = 'zone'
-
-if hasattr(DART_AD_DEFAULTS, 'dfp_id'):
-    DEFAULT_ID = DART_AD_DEFAULTS['dfp_id']
-    del(DART_AD_DEFAULTS['dfp_id'])
-else:
-    DEFAULT_ID = None
+class AdException(Exception):
+    pass
 
 class Ad(object):
-    """ Base class for Ad and Ad_Factory, keeps track of assigned attributes """
+    """
+    Contains all data for a specific ad unit.
+    """
 
     default_site = DEFAULT_SITE
     default_zone = DEFAULT_ZONE
-    default_id = DEFAULT_ID
+    default_dfp_id = DEFAULT_DFP_ID
 
-    def __init__(self, pos, size='0x0', desc_text='', template='ad.html',**kwargs):
+    def __init__(self, pos, size=[[0,0]], desc_text='', template='ad.html',**kwargs):
+        if size is [[0,0]]:
+            raise AdException("Size must be defined in all ad units.")
 
-        try:
-            self.site = kwargs['site']
-            del(kwargs['site'])
-        except KeyError:
-            self.site = self.default_site
-
-        try:
-            self.zone = kwargs['zone']
-            del(kwargs['zone'])
-        except KeyError:
-            self.zone = self.default_zone
-
-        try:
-            self.dfp_id = kwargs['dfp_id']
-            del(kwargs['dfp_id'])
-        except:
-            self.dfp_id = self.default_id
-
+        # Set site, zone, dfp_id.
+        for key in ['site', 'zone', 'dfp_id']:
+            try:
+                setattr(self, key, kwargs[key])
+                del(kwargs[key])
+            except KeyError:
+                setattr(self, key, getattr(self, 'default_{0}'.format(key)))
 
         self.desc_text = desc_text
         self.template = template
 
         self.attributes = {}
         self.attributes.update(kwargs)
-        self.attributes['pos'] = pos
-        self.attributes['sz'] = size
+        self.attributes["pos"] = pos
+
+        self.pos = pos
+        self.size = size
 
     def get_zone(self):
         return self._zone
@@ -97,16 +86,39 @@ class Ad(object):
             formatted = u'%s%s' % (formatted, self._format_value(attr, val))
         return formatted
 
+    def get_dict(self):
+        """
+        Get the specially formatted dict that will be transformed into an
+        ad_slot definition.
+        """
+        attributes = {}
+        for key, value in self.attributes.iteritems():
+
+            # We need to cast all iterables into lists of strings.
+            if isinstance(value, QuerySet):
+                value = [str(x) for x in value]
+
+            attributes[key] = value
+
+        return ("ad-{0}".format(self.pos), {
+                    'sizes': self.size,
+                    'zone': "/{0}/{1}/{2}".format(self.dfp_id, self.site, self.zone),
+                    'properties': attributes,
+                    })
+
+    def get_noscript(self):
+        """
+        Build the noscript string.
+        """
+        pass
+
     def __unicode__(self):
         """ Prints out the Ad using the ad.html template """
 
-        link = self.get_link()
         t = get_template(self.template)
         c = Context({
-            'pos': self.attributes['pos'],
-            'link': link,
-            'dfp_id': '/%s' % self.dfp_id if self.dfp_id is not None else '',
-            'desc_text': self.desc_text
+            'pos': self.pos,
+            'noscript': self.get_noscript()
         })
         return t.render(c)
 
@@ -114,11 +126,11 @@ class AdFactory(object):
 
     attributes = {}
     default_attributes = DART_AD_DEFAULTS
+    ad_slots = {}
 
     def __init__(self, **kwargs):
         self.attributes = self.default_attributes.copy()
         self.set(**kwargs)
-
 
     def set(self, **kwargs):
         self.attributes.update(kwargs)
@@ -129,5 +141,7 @@ class AdFactory(object):
         attr.update(kwargs)
 
         ad = Ad(*args, **attr)
+        ad_slot_key, ad_slot_def = ad.get_dict()
+        self.ad_slots[ad_slot_key] = ad_slot_def
 
         return ad
